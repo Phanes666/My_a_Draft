0cc3ce3996f4094589d9cb3c23a8ca8bde30adeb
Phanes666 <mephistophelesofua@gmail.com>
2023-06-18 13:51:11 +0300

    1111

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+function isAdult(age) {
+    // Change code below this line
+    const passed = age >= 18;
+  console.log(age >= passed);
+    // Change code above this line
+    return passed;
+  }
+  
+  isAdult(20)
+  isAdult(14)
+  isAdult(8)
+  isAdult(37)

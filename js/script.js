2befe61ff8cb6eb8e68da12b1181e9e67d26d260
Phanes666 <mephistophelesofua@gmail.com>
2023-06-18 13:58:14 +0300

    fix-draft

--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@
 function isAdult(age) {
     // Change code below this line
     const passed = age >= 18;
-  console.log(age >= passed);
+//   console.log(age >= passed);
     // Change code above this line
     return passed;
   }
   
-  isAdult(20)
-  isAdult(14)
-  isAdult(8)
-  isAdult(37)
+  console.log(isAdult(20));
+  console.log(isAdult(14));
+  console.log(isAdult(8));
+  console.log(isAdult(37));
+
+
+//   
